775963d8f392fd79458d9a048ded8a78b4d9b7ee
Gabriel Freitas <contatofreitas.gab@hotmail.com>
2025-06-27 21:38:31 -0300

    primeiras aulas, introdução a linguagem e lógica

---
+++ 00 - Fundamentos/comentario.js
@@ -0,0 +1,8 @@
+// console.log() função de exibição textual
+console.log("Comentário não aparecem ao rodar o código.");
+console.log("Para fazer um comentário, basta digitar // e escrever o que quiser.");
+/*
+ * Comentário de 
+ * múltiplas
+ * linhas.
+ */
---
+++ 00 - Fundamentos/sentenca_1.js
@@ -0,0 +1,2 @@
+console.log("Hello World!");
+console.log("Minhas primeiras linhas em JavaScript");
