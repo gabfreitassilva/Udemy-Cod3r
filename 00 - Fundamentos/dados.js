f2685c3e1068318448566245c84910588d3bd62e
Gabriel Freitas <contatofreitas.gab@hotmail.com>
2025-06-29 14:41:18 -0300

    um pouco de variáveis

---
+++ 00 - Fundamentos/dados.js
@@ -0,0 +1,10 @@
+var quantidade = 10;  // declaração e atribuição de valor á variáveis
+var preco = 2.4;
+var desconto = 0.6;
+var precoFinal = preco - (desconto * preco);
+
+console.log("Caneta");
+console.log(quantidade);
+console.log(preco);
+console.log(desconto);
+console.log(precoFinal);
